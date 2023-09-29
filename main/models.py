ec7fb1fa712541533a03f9f0b711ed7c016eb913
salahsaeed19 <salahsaef40@gmail.com>
2023-09-29 19:35:01 +0300

    Update list bost and ass button and add file requirements.txt

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Message(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    message = models.TextField()
+    message = models.TextField(max_length=5000)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
